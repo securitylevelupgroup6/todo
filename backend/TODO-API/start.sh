1c6fe08e9ccf381b297cba9c3b401797ba475dc8
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 21:16:55 +0200

    last fix hopefully

--- backend/TODO-API/start.sh
+++ backend/TODO-API/start.sh
@@ -13,7 +13,7 @@ echo "Downloading and installing flyway"
 wget -q https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/10.11.0/flyway-commandline-10.11.0-linux-x64.tar.gz
 tar -xzf flyway-commandline-10.11.0-linux-x64.tar.gz
 sudo mv flyway-10.11.0 /opt/flyway
-sudo rm /usr/local/bin/flyway
+sudo rm -rf /usr/local/bin/flyway
 sudo ln -s /opt/flyway/flyway /usr/local/bin/flyway
 
 export PATH=$PATH:/usr/local/bin
