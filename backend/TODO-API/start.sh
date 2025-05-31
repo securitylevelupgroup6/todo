9fdc2d528d9aab7c5ffea636572cf39cc925120d
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 20:33:27 +0200

    installing aws cli on the box

--- backend/TODO-API/start.sh
+++ backend/TODO-API/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 sudo apt update && sudo apt install -y jq
+
+# Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+export PATH=$PATH:/usr/local/bin
+
 echo "Fetching DB secrets from AWS Secrets Manager..."
 
 SECRET=$(aws secretsmanager get-secret-value \
