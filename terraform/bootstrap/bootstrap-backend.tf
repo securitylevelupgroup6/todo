a64a883f593fb20907af099940992a2c646aca8f
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 17:54:05 +0200

    fixes

--- terraform/bootstrap/bootstrap-backend.tf
+++ terraform/bootstrap/bootstrap-backend.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket"  
+  bucket = "terraform-state-bucket-security-levelup-grp-6"  
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "dev"
