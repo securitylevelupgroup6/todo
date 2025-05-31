da0c414d0868f8703bd9e82bd7ea6c468ca750c2
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 20:50:43 +0200

    Added migrations

--- backend/TODO-API/start.sh
+++ backend/TODO-API/start.sh
@@ -2,10 +2,16 @@
 set -e
 sudo apt update && sudo apt install -y jq && sudo apt-get install -y unzip
 
-# Install AWS CLI v2
+# install aws
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# download flyway
+curl -L https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/9.22.1/flyway-commandline-9.22.1-linux-x64.tar.gz -o flyway.tar.gz
+tar -xzf flyway.tar.gz
+sudo mv flyway-9.22.1/flyway /usr/local/bin/
+rm -rf flyway.tar.gz flyway-9.22.1
 
 export PATH=$PATH:/usr/local/bin
 
@@ -27,4 +33,9 @@ DB_HOST=$(aws secretsmanager get-secret-value \
 
 CONNECTION_STRING="Host=$DB_HOST;Port=5432;Database=todo-db;Username=$DB_USER;Password=$DB_PASS"
 echo $CONNECTION_STRING
+
+# run flyway migrations 
+flyway -url="jdbc:postgresql://$DB_HOST:5432/todo-api" -user="$DB_USER" -password="$DB_PASS" -locations=filesystem:~/migrations \ migrate
+
+# start the process as a systemd service
 
