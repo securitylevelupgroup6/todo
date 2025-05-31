cc975bfe6a7f669de33d265f611048a1a3fc0914
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 17:58:04 +0200

    removed dynamodb

--- terraform/bootstrap/bootstrap-backend.tf
+++ terraform/bootstrap/bootstrap-backend.tf
@@ -36,24 +36,3 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_block" {
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "Terraform State Lock Table"
-    Environment = "dev"
-  }
-}
-
-
-
-
-
