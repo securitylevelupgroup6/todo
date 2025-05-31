a7bb8535505fd28488d56ef8ddc9e3518e50b807
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 21:22:46 +0200

    fix

--- backend/TODO-API/start.sh
+++ backend/TODO-API/start.sh
@@ -7,16 +7,15 @@ sudo apt update && sudo apt install -y jq && sudo apt-get install -y unzip && su
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -o awscliv2.zip
 sudo ./aws/install --update
+export PATH=$PATH:/usr/local/bin
 
 # download flyway
 echo "Downloading and installing flyway"
-wget -q https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/10.11.0/flyway-commandline-10.11.0-linux-x64.tar.gz
+wget https://download.red-gate.com/maven/release/com/redgate/flyway/flyway-commandline/10.11.0/flyway-commandline-10.11.0-linux-x64.tar.gz
 tar -xzf flyway-commandline-10.11.0-linux-x64.tar.gz
 sudo mv flyway-10.11.0 /opt/flyway
-sudo rm -rf /usr/local/bin/flyway
-sudo ln -s /opt/flyway/flyway /usr/local/bin/flyway
-
-export PATH=$PATH:/usr/local/bin
+export PATH=$PATH:/opt/flyway/flyway-10.11.0
+source ~/.bashrc
 
 echo "Fetching DB secrets from AWS Secrets Manager..."
 
