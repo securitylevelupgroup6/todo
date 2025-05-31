30f428b86e8f0491edc642cd325a408849c7e90e
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-05-31 17:30:45 +0200

    initial terraform and workflows

---
+++ terraform/main/main.tf
@@ -0,0 +1,227 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+data "aws_secretsmanager_secret" "db-secrets" {
+  name = "prod/postgres"
+}
+
+data "aws_secretsmanager_secret_version" "db" {
+  secret_id = data.aws_secretsmanager_secret.db-secrets.id
+}
+
+locals {
+  secret_data = jsondecode(data.aws_secretsmanager_secret_version.db.secret_string)
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_db_subnet_group" "private-group" {
+  name       = "private-group"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  tags = {
+    Name = "Private subnet group"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_security_group" "ec2-security-group" {
+  name        = "ec2-rds-1"
+  description = "Allow ec2 to talk to database"
+  vpc_id      = aws_vpc.main.id
+
+  ingress{
+    from_port = 3000
+    to_port = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds-security-group" {
+    name = "rds-ec2-1"
+    description = "Allow things to talk to rds"
+    vpc_id      = aws_vpc.main.id
+    ingress{
+        from_port = 5432
+        to_port = 5432
+        protocol = "tcp"
+        security_groups = [ aws_security_group.ec2-security-group.id ]
+    }
+}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
+  username             = local.secret_data.db_username
+  password             = local.secret_data.db_password
+  parameter_group_name = "default.postgres15"
+  skip_final_snapshot  = true
+
+  vpc_security_group_ids = [aws_security_group.rds-security-group.id]
+  db_subnet_group_name   = aws_db_subnet_group.private-group.name
+
+  tags = {
+    Name = "test rds instance"
+  }
+}
+
+resource "aws_iam_role" "ec2_secrets_role" {
+  name = "ec2_secrets_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "secrets_access_policy" {
+  name = "SecretsManagerAccessPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "secretsmanager:GetSecretValue"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_secrets_policy" {
+  role       = aws_iam_role.ec2_secrets_role.name
+  policy_arn = aws_iam_policy.secrets_access_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2_instance_profile"
+  role = aws_iam_role.ec2_secrets_role.name
+}
+
+resource "aws_instance" "app_server" {
+  ami  = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+  vpc_security_group_ids = [aws_security_group.ec2-security-group.id]
+}
+
+resource "aws_secretsmanager_secret" "db_hostname" {
+  name = "rds-hostname"
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.db_hostname.id
+
+  secret_string = aws_db_instance.postgres.address
+}
