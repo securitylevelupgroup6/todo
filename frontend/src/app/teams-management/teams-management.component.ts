ed690a481165cd45b085d68d62c607718a32c57c
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-06-02 18:14:43 +0200

    Create and set up angular project

---
+++ frontend/src/app/teams-management/teams-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teams-management',
+  imports: [],
+  templateUrl: './teams-management.component.html',
+  styleUrl: './teams-management.component.scss'
+})
+export class TeamsManagementComponent {
+
+}
