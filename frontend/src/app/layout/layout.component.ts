58624bf7d8eb03b051d3d522f129c3834a3235ea
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2025-06-06 21:02:43 +0200

    Use angular material

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderComponent, SidebarComponent],
+  imports: [CommonModule, RouterModule, HeaderComponent, SidebarComponent, LoginComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   sidebarOpen = false;
+  user: any;
+
+  constructor(
+    private router: Router,
+  ) {}
+  ngOnInit(): void {
+    if(!this.user) {
+      this.router.navigate(['/auth/login']);
+    }
+  }
 } 
