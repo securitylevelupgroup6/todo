9c24a9e3c12cf1bb03c50cb0ac7f9baf42a2822b
Bernard Paetzold <bernard.paetzold@bbd.co.za>
2025-06-10 19:56:07 +0200

    Added endpoint for todo history

--- backend/TODO-API/Mappers/ResponseMappers.cs
+++ backend/TODO-API/Mappers/ResponseMappers.cs
@@ -1,5 +1,4 @@
-﻿using System.Text.Json;
-using TODO_API.Models;
+﻿using TODO_API.Models;
 using TODO_API.Models.Responses;
 
 namespace TODO_API.Mappers;
@@ -38,5 +37,54 @@ public static class ResponseMappers
             Owner = todo.Owner.User.MapToUserResponse(),
             Assignee = todo.TodoState.Assignee?.User.MapToUserResponse(),
         };
+    }
+
+    public static TodoHistoryResponse MapToTodoHistoryResponse(this IEnumerable<TodoHistory> todoHistories, Todo todo)
+    {
+        var response = new TodoHistoryResponse
+        {
+            Todo = todo.MapToTodoResponse(),
+            History = new List<string>()
+        };
+
+        foreach (var history in todoHistories.OrderBy(h => h.Date))
+        {
+            var changes = new List<string>();
+            var oldState = history.OldState;
+            var newState = history.UpdatedState;
+
+            if (oldState != null && oldState.Status?.StatusName != newState.Status?.StatusName)
+            {
+                changes.Add($"state from '{oldState.Status?.StatusName}' to '{newState.Status?.StatusName}'");
+            }
+
+            if (oldState != null && oldState.Title != newState.Title)
+            {
+                changes.Add($"title from '{oldState.Title}' to '{newState.Title}'");
+            }
+
+            if (oldState != null && oldState.Description != newState.Description)
+            {
+                changes.Add($"description from '{oldState.Description}' to '{newState.Description}'");
+            }
+
+            var oldAssignee = oldState?.Assignee?.User;
+            var newAssignee = newState.Assignee?.User;
+            if (oldAssignee?.Id != newAssignee?.Id)
+            {
+                var oldAssigneeName = oldAssignee != null ? $"{oldAssignee.FirstName} {oldAssignee.LastName}" : "none";
+                var newAssigneeName = newAssignee != null ? $"{newAssignee.FirstName} {newAssignee.LastName}" : "none";
+                changes.Add($"assignee from '{oldAssigneeName}' to '{newAssigneeName}'");
+            }
+
+            if (changes.Count > 0)
+            {
+                var username = history.Reporter?.User?.FirstName + ' ' + history.Reporter?.User?.LastName ?? "Unknown";
+                var changeString = $"{username} changed the {string.Join(", ", changes)} on {history.Date:yyyy-MM-dd HH:mm}";
+                response.History.Add(changeString);
+            }
+        }
+
+        return response;
     }
 }
