5d79d13cac40947c013770d8f6a8d490c721d70c
Cade Sayner <Cade.Sayner@bbd.co.za>
2025-06-11 11:45:36 +0200

    another fix

--- backend/TODO-API/start.sh
+++ backend/TODO-API/start.sh
@@ -125,11 +125,8 @@ server {
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
-        location / {
-        proxy_pass http://localhost:5000;
         proxy_set_header Origin \$http_origin;
         proxy_buffering off;
-        }
     }
 
     location / {
